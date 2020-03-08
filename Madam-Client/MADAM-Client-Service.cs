5d01a22611c8a530c610ff7c15a54fdec1675c5b
Jim Hemming <jim.hemming@gmail.com>
2020-03-08 16:45:16 +0000

    listens for network address change

---
+++ Madam-Client/MADAM-Client-Service.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Madam_Client
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkAddressChanged);
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void NetworkAddressChanged(object sender, EventArgs e)
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface n in adapters)
+            {
+                EventLog.WriteEntry("NetworkMonitor", String.Format("{0} is {1}", n.Name, n.OperationalStatus), EventLogEntryType.Warning);
+            }
+        }
+    }
+}
