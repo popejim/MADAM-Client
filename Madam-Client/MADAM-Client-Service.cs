45e01270bfcbbffd050b058982bcdd28764af86b
Jim Hemming <jim.hemming@gmail.com>
2020-04-09 18:55:00 +0100

    debugs in vs, gets ipv4 addresses from all adapters when network address changes

--- Madam-Client/MADAM-Client-Service.cs
+++ Madam-Client/MADAM-Client-Service.cs
@@ -14,6 +14,7 @@ namespace Madam_Client
 {
     public partial class Service1 : ServiceBase
     {
+        public string ipv4;
         public Service1()
         {
             InitializeComponent();
@@ -21,20 +22,41 @@ namespace Madam_Client
 
         protected override void OnStart(string[] args)
         {
+            Console.WriteLine("Service Starting....");
             NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkAddressChanged);
         }
 
         protected override void OnStop()
         {
+            Console.WriteLine("Service Stopping....");
         }
 
         private void NetworkAddressChanged(object sender, EventArgs e)
         {
             NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
             foreach (NetworkInterface n in adapters)
             {
-                EventLog.WriteEntry("NetworkMonitor", String.Format("{0} is {1}", n.Name, n.OperationalStatus), EventLogEntryType.Warning);
+                foreach (UnicastIPAddressInformation ip in n.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        ipv4 = ip.Address.ToString();
+                    }
+                } 
+                Console.WriteLine("NetworkMonitor {0} is {1} IP {2}",n.Name, n.OperationalStatus, ipv4);
             }
+
+            Console.WriteLine(" ");
         }
+
+        internal void TestStartupAndStop(string[] args)
+        {
+            this.OnStart(args);
+            Console.ReadLine();
+            this.OnStop();
+        }
+
+
     }
 }
--- Madam-Client/Program.cs
+++ Madam-Client/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Madam_Client
 {
@@ -14,12 +15,22 @@ namespace Madam_Client
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            if (Environment.UserInteractive)
             {
+                Madam_Client.Service1 service1 = new Madam_Client.Service1();
+                service1.TestStartupAndStop(null);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
                 new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
         }
+
+
     }
 }
